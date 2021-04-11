9a1a3984951d982bfa0cd837646a6a2ae0604b0b
Ruslan Yakimov <goodbuhlnr@yandex.ua>
2021-04-11 18:47:24 +0300

    Stating to wtite code

--- homework.py
+++ homework.py
@@ -1,9 +1,70 @@
-import datetime as dt
+from _datetime import datetime as dt
+
+cash_records = []
+calories_records = []
 
 
 class Calculator:
-    pass
+
+    def __init__(self, amout, limit):
+        # Declaring attributes
+        self.amount = amout
+        self.limit = limit
+
+    def difference(self):
+        # Getting difference
+        dfr_is = self.limit - self.amount
+        return dfr_is
+
+
+class CaloriesCalculator(Calculator):
+
+    def add_record(self):
+        pass
+
+    def get_today_stats(self):
+        pass
+
+    def get_calories_remained(self):
+        pass
+
+    def get_week_stats(self):
+        pass
+
+
+class CashCalculator(Calculator):
+
+    def add_record(self):
+        pass
+
+    def get_today_stats(self):
+        pass
+
+    def get_todat_cash_remained(self, currency):
+        pass
+
+    def get_week_stats(self):
+        pass
 
 
 class Record:
-    pass
+    def __init__(self, amount, comment, date):
+        self.amount = amount
+        self.comment = comment
+        self.date = date
+        Record.add_records(self)
+
+    def add_records(self):
+        record = {'amount': self.amount,
+                  'comment':self.comment,
+                  'date': self.date}
+        records.append(record)
+
+
+r1 = Record(amount=145, comment='Безудержный шопинг', date='08.03.2019')
+r2 = Record(amount=1568,
+            comment='Наполнение потребительской корзины',
+            date='09.03.2019')
+r3 = Record(amount=691, comment='Катание на такси', date='08.03.2019')
+
+print(records)
